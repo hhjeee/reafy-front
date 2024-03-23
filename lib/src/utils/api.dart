ac141e5802fb556f350ec9182891d516ed62a941
hhjeee <yoonhj0315@gmail.com>
2024-03-23 16:08:26 +0900

    feat: 최근 독서 조회 api 추가

--- lib/src/utils/api.dart
+++ lib/src/utils/api.dart
@@ -106,7 +106,7 @@ class authDio {
         final newAccessToken = refreshResponse.data['accessToken'];
         await prefs.setString('token', newAccessToken);
         print(
-            "[***] Token refresh successful "); //with newAccessToken: $newAccessToken");
+            "[***] Token refresh successful with newAccessToken: $newAccessToken"); //with newAccessToken: $newAccessToken");
         return newAccessToken;
       } else {
         print(
@@ -127,15 +127,15 @@ class authDio {
 class CustomLogInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    print('REQUEST[${options.method}] => PATH: ${options.path}');
+    //print('REQUEST[${options.method}] => PATH: ${options.path}');
     super.onRequest(options, handler);
   }
 
   @override
   void onResponse(
       Response<dynamic> response, ResponseInterceptorHandler handler) {
-    print(
-        'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
+    // print(
+    //     'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
     super.onResponse(response, handler);
   }
 
