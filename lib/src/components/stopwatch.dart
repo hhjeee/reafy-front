0932037d604685d9557db92e90d2f096e47a0fd3
hhjeee <yoonhj0315@gmail.com>
2024-03-23 18:13:44 +0900

    chore: 주석수정

--- lib/src/components/stopwatch.dart
+++ lib/src/components/stopwatch.dart
@@ -9,7 +9,7 @@ class StopwatchWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     StopwatchProvider stopwatch = Provider.of<StopwatchProvider>(context);
-    stopwatch.setTimer(1 * 60); // 시간설정
+    stopwatch.setTimer(15 * 60); // 시간설정
 
     void _tapStopwatch(Status status) async {
       switch (status) {
--- lib/src/utils/api.dart
+++ lib/src/utils/api.dart
@@ -127,15 +127,15 @@ class authDio {
 class CustomLogInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    //print('REQUEST[${options.method}] => PATH: ${options.path}');
+    print('REQUEST[${options.method}] => PATH: ${options.path}');
     super.onRequest(options, handler);
   }
 
   @override
   void onResponse(
       Response<dynamic> response, ResponseInterceptorHandler handler) {
-    // print(
-    //     'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
+    print(
+        'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
     super.onResponse(response, handler);
   }
 
@@ -170,7 +170,8 @@ Future<void> _showLoginExpiredDialog(BuildContext context) async {
             Center(
               child: ElevatedButton(
                   onPressed: () {
-                    Provider.of<AuthProvider>(context, listen: false).logout();
+                    Provider.of<AuthProvider>(context, listen: false)
+                        .logout(context);
                     //prefs.clear();
                     Get.off(() => LoginPage());
                     //Navigator.pop(context);
