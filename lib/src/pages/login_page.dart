3d490ecb11610c30bf25ca4eebc4fea6b17de255
jeeho <jiho@jeeehos-MacBook-Air.local>
2024-07-16 15:11:38 +0900

    [fix] 네트워크 경고알림 제거

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -88,7 +88,7 @@ class _LoginPageState extends State<LoginPage> {
         Get.off(() => LoginPage());
       }
     } else {
-      showNetworkErrorSnackbar(context);
+      //showNetworkErrorSnackbar(context);
       //Get.off(() => LoginPage());
     }
   }
